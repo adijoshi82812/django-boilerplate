97ba67d604cfc829cbad6f92d4f3483138c4fd09
Aditya Joshi <adijoshi82812@pop-os.localdomain>
2021-08-09 05:09:14 +0530

    Added the rename command

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 INSTALLED_APPS = [
+    "core.apps.CoreConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
